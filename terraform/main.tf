a090ca4b914a4a0697f3e146740762e6ebf83061
Carlos Tapia <carlos.tapia@tecsup.edu.pe>
2026-03-18 13:10:20 -0500

    Workflow completo CI/CD4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.77"  # versión actual que soporta Developer y Consumption
+    }
+  }
+  required_version = ">= 1.5.0"
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
